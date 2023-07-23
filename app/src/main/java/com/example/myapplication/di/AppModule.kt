a4111ef9b1a7bb388a512ea7032e9aad093da6a5
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-07-23 17:13:50 +0530

    modified

---
+++ app/src/main/java/com/example/myapplication/di/AppModule.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.di
+
+import android.app.Application
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+
+class AppModule {
+
+}
